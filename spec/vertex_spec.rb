f27c500ab0e6a1c49de1e07bd37b791e11b92c02
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 00:20:24 -0700

    create context for Vertex rspec

---
+++ spec/vertex_spec.rb
@@ -0,0 +1,11 @@
+require 'vertex'
+
+describe Vertex do
+  let(:vertex) {Vertex.new({'x'=> 0,'y'=> 0, 'z'=> 0})}
+  context "#initialize" do
+  end
+  context "#move!" do
+  end
+  context "#to_s" do
+  end
+end
