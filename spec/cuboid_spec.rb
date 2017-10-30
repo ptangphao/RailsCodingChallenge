31ecf88bb486feaa1d0a4216cb7de48fbe72f7cd
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 02:00:57 -0700

    complete tests for Cuboid#origin

--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -13,28 +13,33 @@ describe Cuboid do
     end
 
     it "requires positive integers as dimension parameters" do
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 0, 'height' => 5, 'width' => 5}}.to raise_error(RuntimeError)
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => -1, 'height' => 5, 'width' => 5}}.to raise_error(RuntimeError)
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 0, 'width' => 5}}.to raise_error(RuntimeError)
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => -1, 'width' => 5}}.to raise_error(RuntimeError)
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 5, 'width' => 0}}.to raise_error(RuntimeError)
-      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 5, 'width' => -1}}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 0, 'height' => 5, 'width' => 5})}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => -1, 'height' => 5, 'width' => 5})}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 0, 'width' => 5})}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => -1, 'width' => 5})}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 5, 'width' => 0})}.to raise_error(RuntimeError)
+      expect{Cuboid.new({'x'=> 0,'y'=> 1, 'z'=> 2, 'length' => 5, 'height' => 5, 'width' => -1})}.to raise_error(RuntimeError)
     end    
   end
 
   context "#origin" do
-    it "should return a Vertex object" do
+    it "returns a Vertex object" do
       expect(cuboid.origin).to be_an_instance_of Vertex
     end
 
-    it "should return a Vertex with values matching initialization parameters" do
+    it "returns a Vertex with values matching initialization parameters" do
       expect(cuboid.origin.x = 0).to be_true
       expect(cuboid.origin.y = 1).to be_true
       expect(cuboid.origin.z = 2).to be_true
     end
+
+    it "cannot be directly modified" do
+      expect{cuboid.origin = Vertex.new({'x'=> 0,'y'=> 1, 'z'=> 2}}.to raise_error(NoMethodError)
+    end
   end
 
   context "#length" do
+    
   end
 
   context "#width" do
