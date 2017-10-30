48a11150cce8f46177301272751f59df905b94a3
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 00:55:56 -0700

    specify readability for instance variables in Vertex class

--- lib/vertex.rb
+++ lib/vertex.rb
@@ -2,6 +2,8 @@ class Vertex
 
   REQUIRED_COORDINATES = ['x','y','z']
 
+  attr_reader :x, :y, :z
+
   def initialize(params = {})
     # params_check(params)
     @x, @y, @z = params['x'], params['y'], params['z']
