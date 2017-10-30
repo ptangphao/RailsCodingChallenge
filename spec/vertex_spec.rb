390686f1a7e0dbc70ed79e39ecc0bf07b47217bb
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 00:42:43 -0700

    create rspec tests for x,y,z instance variables in Vertex class

--- spec/vertex_spec.rb
+++ spec/vertex_spec.rb
@@ -1,7 +1,7 @@
 require 'vertex'
 
 describe Vertex do
-  let(:vertex) {Vertex.new({'x'=> 0,'y'=> 0, 'z'=> 0})}
+  let(:vertex) {Vertex.new({'x'=> 0,'y'=> 1, 'z'=> 2})}
   
   context "#initialize" do   
     it "creates a Vertex object" do
@@ -21,16 +21,31 @@ describe Vertex do
 
   context "#x" do
     it "should return the x-axis value" do
+      expect(vertex.x).to eq 0
+    end
+
+    it "can not be modified" do
+      expect(vertex.x = 4).to raise_error
     end
   end
 
   context "#y" do
     it "should return the y-axis value" do
+      expect(vertex.y).to eq 1
+    end
+
+    it "can not be modified" do
+      expect(vertex.y = 4).to raise_error
     end
   end
 
   context "#z" do
     it "should return the z-axis value" do
+      expect(vertex.z).to eq 2
+    end
+
+    it "can not be modified" do
+      expect(vertex.z = 4).to raise_error
     end
   end
 
