29989d643006a8643fd2666909677acea3c76eb3
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 00:36:11 -0700

    create vertex initialize parameter rspecs

--- spec/vertex_spec.rb
+++ spec/vertex_spec.rb
@@ -2,8 +2,28 @@ require 'vertex'
 
 describe Vertex do
   let(:vertex) {Vertex.new({'x'=> 0,'y'=> 0, 'z'=> 0})}
-  context "#initialize" do
+  
+  context "#initialize" do   
+    it "creates a Vertex object" do
+      expect(vertex).to be_an_instance_of Vertex 
+    end
+
+    it "requires parameters" do
+      expect(Vertex.new).to raise_error
+    end
+
+    it "requires non-negative integers as parameters" do
+      expect(Vertex.new({'x'=> -1,'y'=> 0, 'z'=> 0}).to raise_error
+      expect(Vertex.new({'x'=> 0,'y'=> -1, 'z'=> 0}).to raise_error
+      expect(Vertex.new({'x'=> 0,'y'=> 0, 'z'=> -1}).to raise_error
+    end
   end
+
+  context "#x" do
+    it "should return the x-axis value" do
+    end
+  end 
+
   context "#move!" do
   end
   context "#to_s" do
