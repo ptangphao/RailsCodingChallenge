588de359638b05553c234dd5e9a7947679eab188
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-10-30 02:39:20 -0700

    Complete vertices method for Cuboid class, pass rspec

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -15,4 +15,11 @@ class Cuboid
   def move_to!(x, y, z)
     @origin.move!(x,y,z)
   end
+
+  def vertices
+    x_vals, y_vals, z_vals = [@origin.x, @origin.x + @height], [@origin.y, @origin.y + @width], [@origin.z, @origin.z + @length]
+    vertices_formulaes = x_vals.product(y_vals, z_vals)
+    vertices_list = vertices_formulaes.map{|vertex| Vertex.new({'x' => vertex[0], 'y' => vertex[1], 'z' => vertex[2]})}
+    return vertices_list
+  end
 end
